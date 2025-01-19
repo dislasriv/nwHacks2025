726e9a2b75794505920da59a6b3e5af6d449052d
Bruce Blore <bruceblore@protonmail.com>
2025-01-19 01:54:04 -0800

    Unfuck the merge

--- session_info/session_info.js
+++ session_info/session_info.js
@@ -1,43 +1,21 @@
-
-
 //Fires when HTML has been completely loaded and parsed
-document.addEventListener('DOMContentLoaded', () => {
-  //HTML component website-list on popup.html
-  const websiteList = document.getElementById('website-list');
-
+document.addEventListener('DOMContentLoaded', async () => {
   //get websiteTimes OBJECT
-  chrome.storage.local.get(['websiteTimes'], (result) => {
-    // assign result.webtimes OR an empty object if that is not defined
-    // result is the object returned when 'websiteTimes' is queried
-    websiteTimes = result.websiteTimes || {};
+  let result = await chrome.storage.local.get(['websiteTimes']);
+  // assign result.webtimes OR an empty object if that is not defined
+  // result is the object returned when 'websiteTimes' is queried
+  let websiteTimes = result.websiteTimes || {};
 
-    //object.entries returns a list of the key-value pairs that website times is composed of...
-    //const [url, time] deconstructs these pairs into two vars
-    for (const [url, time] of Object.entries(websiteTimes)) {
-      //add element to body of HTML (li element)
-      const listItem = document.createElement('li');
+  //object.entries returns a list of the key-value pairs that website times is composed of...
+  //const [url, time] deconstructs these pairs into two vars
+  for (const [url, time] of Object.entries(websiteTimes)) {
+    AddRowToTable(url, time)
+  }
 
-      //process the URL
-      formatUrl = url.split("/");
-      formatUrl = formatUrl[1];
-      //compose the text
-      listItem.textContent = `${formatUrl}: ${time} minutes`;
-      //add new HTML element to website-list (id) HTML object
-      websiteList.appendChild(listItem);
-    }
-  });
-
-
-  //EVENT LISTENERS:
-  settingsButton.addEventListener("click", () => {
-    location.href = "/settings/settings.html";
-  });
   //EVENT LISTENERS:
   const settingsButton = document.getElementById('settings-button');
   settingsButton.addEventListener("click", () => {
-    window.close();
-    chrome.alarms.create('screenTransition', { delayInMinutes: 0.001 });
-    chrome.action.setPopup({ popup: "settings/settings.html" });
+    location.href = "/settings/settings.html";
   });
 
 });
