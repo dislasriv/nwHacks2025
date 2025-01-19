9f597d7c39285ba00c69d86b9101a12a24cbd5c1
Stella Zarei <zareistella@gmail.com>
2025-01-18 20:16:29 -0800

    Correct formatting

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 console.log("hello i am functioning and normal c:");
 
 //addListener in any any context takes a function as a parameter: the event handler
-chrome.runtime.onStartup.addListener(function(){
+chrome.runtime.onStartup.addListener(function () {
     console.log("browser was opened");
 
     chrome.notifications.create({
@@ -10,43 +10,43 @@ chrome.runtime.onStartup.addListener(function(){
         title: "Welcome Back!",
         message: "Your browser just started.",
         priority: 2,
-      })
+    })
 })
 
 chrome.alarms.create('timer', { periodInMinutes: 1 });
 
 chrome.alarms.onAlarm.addListener(async (alarm) => {
     if (alarm.name === 'timer') {
-      const activeTab = await chrome.tabs.query({ active: true, currentWindow: true });
-      const domainRegex = /\/[^\/]+\//g;
+        const activeTab = await chrome.tabs.query({ active: true, currentWindow: true });
+        const domainRegex = /\/[^\/]+\//g;
 
-      //will error if user is doing split screen, or does not have chrome open at the moment (ie: active tab isnt defined)
-      const url = domainRegex.exec(activeTab[0].url);
-     // a time limit in minutes for when the popup should occur
-      const limit = 1;
-    
-      //get 'websiteTimes' object and run the arrow function on it
-      chrome.storage.local.get(['websiteTimes'], async (result) => {
-        // assign result.webtimes OR an empty dict if that is not defined
-        let websiteTimes = result.websiteTimes || {};
-        
-        //instantiate/increment websiteTimes[url] keyval pair
-        if (websiteTimes[url]) {
-          websiteTimes[url] += 1;
-        } else {
-          websiteTimes[url] = 1;
-        }
-        // log time in minutes spent
-        console.log(websiteTimes[url]);
+        //will error if user is doing split screen, or does not have chrome open at the moment (ie: active tab isnt defined)
+        const url = domainRegex.exec(activeTab[0].url);
+        // a time limit in minutes for when the popup should occur
+        const limit = 1;
 
-        //at limit make popup
-        if(websiteTimes[url] == limit) {
-            chrome.action.setPopup({popup: "popup/session_info.html"});
-            chrome.action.openPopup();
-        }
-        
-        //asynchonous call to store "websiteTimes":websiteTimes on local storage (ie:update the WebsiteTimes structure)
-        await chrome.storage.local.set({ websiteTimes });
-      });
+        //get 'websiteTimes' object and run the arrow function on it
+        chrome.storage.local.get(['websiteTimes'], async (result) => {
+            // assign result.webtimes OR an empty dict if that is not defined
+            let websiteTimes = result.websiteTimes || {};
+
+            //instantiate/increment websiteTimes[url] keyval pair
+            if (websiteTimes[url]) {
+                websiteTimes[url] += 1;
+            } else {
+                websiteTimes[url] = 1;
+            }
+            // log time in minutes spent
+            console.log(websiteTimes[url]);
+
+            //at limit make popup
+            if (websiteTimes[url] == limit) {
+                chrome.action.setPopup({ popup: "popup/session_info.html" });
+                chrome.action.openPopup();
+            }
+
+            //asynchonous call to store "websiteTimes":websiteTimes on local storage (ie:update the WebsiteTimes structure)
+            await chrome.storage.local.set({ websiteTimes });
+        });
     }
-  });
+});
--- popup/session_info.js
+++ popup/session_info.js
@@ -6,23 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //get websiteTimes OBJECT
     chrome.storage.local.get(['websiteTimes'], (result) => {
-      // assign result.webtimes OR an empty object if that is not defined
-          // result is the object returned when 'websiteTimes' is queried
-      const websiteTimes = result.websiteTimes || {};
-      
-      //object.entries returns a list of the key-value pairs that website times is composed of...
-        //const [url, time] deconstructs these pairs into two vars
-      for (const [url, time] of Object.entries(websiteTimes)) {
-        //add element to body of HTML (li element)
-        const listItem = document.createElement('li');
+        // assign result.webtimes OR an empty object if that is not defined
+        // result is the object returned when 'websiteTimes' is queried
+        const websiteTimes = result.websiteTimes || {};
 
-        //process the URL
-        formatUrl =  url.split("/");
-        formatUrl = formatUrl[1];
-        //compose the text
-        listItem.textContent = `${formatUrl}: ${time} minutes`;
-        //add new HTML element to website-list (id) HTML object
-        websiteList.appendChild(listItem);
-      }
+        //object.entries returns a list of the key-value pairs that website times is composed of...
+        //const [url, time] deconstructs these pairs into two vars
+        for (const [url, time] of Object.entries(websiteTimes)) {
+            //add element to body of HTML (li element)
+            const listItem = document.createElement('li');
+
+            //process the URL
+            formatUrl = url.split("/");
+            formatUrl = formatUrl[1];
+            //compose the text
+            listItem.textContent = `${formatUrl}: ${time} minutes`;
+            //add new HTML element to website-list (id) HTML object
+            websiteList.appendChild(listItem);
+        }
     });
-  });
+});
