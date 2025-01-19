51b8634a12e0fe473e0370fe6ac0da7d58285df2
Diego Islas <boogeyif@gmail.com>
2025-01-18 17:23:46 -0800

    Alarm and timer added to bring up popup

--- background.js
+++ background.js
@@ -1,15 +1,31 @@
 console.log("hello i am functioning and normal c:");
 
+
+
 //addListener in any any context takes a function as a parameter: the event handler
 chrome.runtime.onStartup.addListener(function(){
     console.log("browser was opened");
 
-    //
+
+    //create alarm
+        //-DelayInSeconds => time to first ping
+        //-PeriodInSeconds => time to repeat beyond that
+    chrome.alarms.create("periodicalPing", {delayInMinutes:1, periodInMinutes:1});
+
+    //notifications are little boxes that appear in bottom right of screen with an icon, title and message
     chrome.notifications.create({
         type: "basic",
         iconUrl: "images/test.png",
         title: "Welcome Back!",
-        message: "Your browser just started. Ready to take on the day?",
+        message: "Your browser just started. ",
         priority: 2,
-      })
-})
+      });
+
+    
+});
+
+//alarm handler
+// Listen for the alarm event
+chrome.alarms.onAlarm.addListener((alarm) => {
+    console.log("BEAYACH");
+  });
