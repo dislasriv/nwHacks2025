b491ca4ff80ece019f1c853fdd74e2524676749c
Allison <amluna@student.ubc.ca>
2025-01-18 17:05:44 -0800

    times sites, popsup every 1min

--- background.js
+++ background.js
@@ -13,3 +13,34 @@ chrome.runtime.onStartup.addListener(function(){
         priority: 2,
       })
 })
+
+chrome.alarms.create('timer', { periodInMinutes: 1 });
+
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+    if (alarm.name === 'timer') {
+      const activeTab = await chrome.tabs.query({ active: true, currentWindow: true });
+
+      const domainRegex = /\/[^\/]+\//g;
+
+      const url = domainRegex.exec(activeTab[0].url);
+
+      const limit = 7;
+  
+      chrome.storage.local.get(['websiteTimes'], async (result) => {
+        let websiteTimes = result.websiteTimes || {};
+  
+        if (websiteTimes[url]) {
+          websiteTimes[url] += 1;
+        } else {
+          websiteTimes[url] = 1;
+        }
+        console.log(websiteTimes[url]);
+        if(websiteTimes[url] == limit) {
+            chrome.action.setPopup("popup.html");
+            chrome.action.openPopup();
+        }
+  
+        await chrome.storage.local.set({ websiteTimes });
+      });
+    }
+  });
