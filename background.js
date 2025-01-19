b06683654187904fc2e1c744040ac43b9addd11f
Diego Islas <boogeyif@gmail.com>
2025-01-18 16:21:58 -0800

    Added popup on start event to background.js

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
 console.log("hello i am functioning and normal c:");
+
+//addListener in any any context takes a function as a parameter: the event handler
+chrome.runtime.onStartup.addListener(function(){
+    console.log("browser was opened");
+
+    //
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: "images/test.png",
+        title: "Welcome Back!",
+        message: "Your browser just started. Ready to take on the day?",
+        priority: 2,
+      })
+})
